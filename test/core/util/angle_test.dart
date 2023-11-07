c671066373e0e88414bf0cf7460dcb51eff2942e
Qazeem Abiodun <qudusqazeem@gmail.com>
2023-11-07 18:19:09 +0100

    initiate test

---
+++ test/core/util/angle_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ubenwa/core/core.dart';
+
+void main() {
+  group("Angle test", () {
+    test("Verify conversion to radian is correct", () {
+      final angle = Angle(90).toRadians;
+      expect(angle, isA<double>());
+      expect(angle, greaterThanOrEqualTo(1.57));
+    });
+
+    test("Verify conversion to  degree is correct", () {
+      final angle = Angle(1.57).toDegrees;
+      expect(angle, isA<num>());
+      expect(angle.round(), equals(90));
+    });
+  });
+}
